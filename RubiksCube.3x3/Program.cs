f41f9d16ee09e35b500abffcb9c0f9e1da371b5b
Ali Hack <9570285+hasack@users.noreply.github.com>
2025-09-14 23:07:59 +0100

    Started to add content into console app.

--- RubiksCube.3x3/Program.cs
+++ RubiksCube.3x3/Program.cs
@@ -1,9 +1,43 @@
-﻿namespace RubiksCube.Cube3x3;
+﻿using RubiksCube.Cube3x3.Models;
+using RubiksCube.Library.Enums;
+
+namespace RubiksCube.Cube3x3;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var cube = SetupCube();
+    }
+
+    private static Cube3x3x3 SetupCube()
+    {
+        var cube = new Cube3x3x3()
+            .AddTop(new CubeFace3x3(
+                Square.White, Square.White, Square.White,
+                Square.White, Square.White, Square.White,
+                Square.White, Square.White, Square.White))
+            .AddFront(new CubeFace3x3(
+                Square.Red, Square.Red, Square.Red,
+                Square.Red, Square.Red, Square.Red,
+                Square.Red, Square.Red, Square.Red))
+            .AddLeft(new CubeFace3x3(
+                Square.Blue, Square.Blue, Square.Blue,
+                Square.Blue, Square.Blue, Square.Blue,
+                Square.Blue, Square.Blue, Square.Blue))
+            .AddRight(new CubeFace3x3(
+                Square.Green, Square.Green, Square.Green,
+                Square.Green, Square.Green, Square.Green,
+                Square.Green, Square.Green, Square.Green))
+            .AddBack(new CubeFace3x3(
+                Square.Orange, Square.Orange, Square.Orange,
+                Square.Orange, Square.Orange, Square.Orange,
+                Square.Orange, Square.Orange, Square.Orange))
+            .AddBottom(new CubeFace3x3(
+                Square.Yellow, Square.Yellow, Square.Yellow,
+                Square.Yellow, Square.Yellow, Square.Yellow,
+                Square.Yellow, Square.Yellow, Square.Yellow));
+
+            return cube;
     }
 }
